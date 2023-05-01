c0161353e684cad96aae4108c996a09f467bc121
vagabond3 <20r01a6665@cmritonline.ac.in>
2023-05-01 21:40:45 +0530

    fswd lab programs

---
+++ week6/w6jquery.js
@@ -0,0 +1,23 @@
+
+var images = ['a.jpg', 'b.jpg', 'c.jpg'];
+
+var x = 0;
+
+var imgs = document.getElementById('img');
+
+setInterval(slider, 2000);
+
+
+function slider() {
+
+  if (x < images.length) {
+    x = x + 1;
+  } else {
+    x = 1;
+  }
+
+
+  imgs.innerHTML = "<img src=" + images[x - 1] + ">";
+
+
+}
